95b22c2cc0e4e5b5f93f3fce7fb72dcd2ecd8f55
alex <al@exmaple.ru>
2015-05-01 17:24:53 +0300

    test

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-// Задаем константы:
+// Задаем константы1:
 define ('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
 $sitePath = realpath(dirname(__FILE__) . DS) . DS;
 define ('SITE_PATH', $sitePath); // путь к корневой папке сайта
